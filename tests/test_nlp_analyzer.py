2d4b62dea26d51dfccabbc581da72a2b95d51aaf
Wiley Hunt <johnhunt@dhcp-wifi-8021x-168-122-7-219.bu.edu>
2021-02-12 09:57:15 -0500

    Test adding trace malloc

--- tests/test_nlp_analyzer.py
+++ tests/test_nlp_analyzer.py
@@ -4,6 +4,16 @@ import tracemalloc
 import cProfile
 import re
 
+# Start trace malloc
+tracemalloc.start()
 # Dummy test to test workflow
 def test_placeholder():
     assert "helloworld" == "helloworld"
+
+# Get snapshot
+snapshot = tracemalloc.take_snapshot()
+top_stats = snapshot.statistics('lineno')
+
+print("[ Top 10 ]")
+for stat in top_stats[:10]:
+    print(stat)
