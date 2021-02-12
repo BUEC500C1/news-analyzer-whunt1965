1e8796ed10dffe8d2a39723150be4d3baa9db7f6
Wiley Hunt <johnhunt@MacBook-Pro.local>
2021-02-12 13:35:58 -0500

    Trying another memory profiler

--- tests/test_newsfeed_ingester.py
+++ tests/test_newsfeed_ingester.py
@@ -1,4 +1,5 @@
 # Tests for newsfeed module
+from memory_profiler import profile
 
 import tracemalloc
 import cProfile
@@ -6,5 +7,9 @@ import re
 
 
 # Dummy test to test workflow
+@profile
 def test_placeholder():
     assert "helloworld" == "helloworld"
+
+if __name__ == '__main__':
+    test_placeholder()
