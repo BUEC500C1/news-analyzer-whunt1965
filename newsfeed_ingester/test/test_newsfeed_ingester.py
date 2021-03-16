fe9b7c0ce8a999c1359535bfd9c381dbe05232cb
Wiley Hunt <johnhunt@MacBook-Pro.local>
2021-03-16 15:12:22 -0400

    Testing whether fileuploader tests work by themselves

--- newsfeed_ingester/test/test_newsfeed_ingester.py
+++ newsfeed_ingester/test/test_newsfeed_ingester.py
@@ -1,92 +1,92 @@
-# Tests for newsfeed module
-from memory_profiler import profile
-
-import tracemalloc
-import cProfile
-import re
-from newsfeedingester import *
-import json
-
-
-# ==================
-# Start Tests
-# ==================
-
-# Dummy test to test workflow
-@profile
-def test_keyword_query():
-
-    #Valid Tests
-    validquery1 = "hats"
-    Result = keyword_query(validquery1)
-    assert type(Result) == list
-    validquery2 = ["hats", "bananas", "apples"]
-    Result = keyword_query(validquery2)
-    assert type(Result) == list
-
-    #Invalid Test
-    invalidquery1 = 1
-    Result = keyword_query(invalidquery1)
-    assert Result == []
-    invalidquery2 = []
-    Result = keyword_query(invalidquery2)
-    assert Result == []
-
-@profile
-def test_person_query():
-    #Valid Tests
-    Result = person_query("Osama Alshaykh")
-    assert type(Result) == list
-    Result = person_query("Javier Bardim")
-    assert type(Result) == list
-
-    #Invalid Test
-    Result = person_query(1)
-    assert Result == []
-    Result = person_query(["hats"])
-    assert Result == []
-
-@profile
-def test_historical_query():
-    #Valid Tests
-    Result = historical_query("1970", "June", ["Oil"])
-    assert type(Result) == list
-    Result = historical_query("1980", "January", ["Bonds"])
-    assert type(Result) == list
-
-    #Invalid Test
-    Result = historical_query(1, 2, "cow")
-    assert Result == []
-    Result = historical_query(1980, "June", "Covid")
-    assert Result == []
-    Result = historical_query("1980", "June", [])
-    assert Result == []
-
-# ==================
-# Ends Tests
-# ==================
-
-
-#Print header for report
-def printTitle():
-    print()
-    print("===NEWS FEED TESTS===")
-    print()
-
-# Include all test functions here to use memory and CPU profilers
-def main():
-    printTitle()
-    tracemalloc.start()# Start trace malloc
-    test_keyword_query()
-    test_person_query()
-    test_historical_query() 
-
-    # Get snapshot
-    snapshot = tracemalloc.take_snapshot()
-    top_stats = snapshot.statistics('lineno')
-    print("[ Top 5 ]")
-    for stat in top_stats[:5]:
-        print(stat)
-
-if __name__ == '__main__':
-    cProfile.run('main()')
+# # Tests for newsfeed module
+# from memory_profiler import profile
+#
+# import tracemalloc
+# import cProfile
+# import re
+# from newsfeedingester import *
+# import json
+#
+#
+# # ==================
+# # Start Tests
+# # ==================
+#
+# # Dummy test to test workflow
+# @profile
+# def test_keyword_query():
+#
+#     #Valid Tests
+#     validquery1 = "hats"
+#     Result = keyword_query(validquery1)
+#     assert type(Result) == list
+#     validquery2 = ["hats", "bananas", "apples"]
+#     Result = keyword_query(validquery2)
+#     assert type(Result) == list
+#
+#     #Invalid Test
+#     invalidquery1 = 1
+#     Result = keyword_query(invalidquery1)
+#     assert Result == []
+#     invalidquery2 = []
+#     Result = keyword_query(invalidquery2)
+#     assert Result == []
+#
+# @profile
+# def test_person_query():
+#     #Valid Tests
+#     Result = person_query("Osama Alshaykh")
+#     assert type(Result) == list
+#     Result = person_query("Javier Bardim")
+#     assert type(Result) == list
+#
+#     #Invalid Test
+#     Result = person_query(1)
+#     assert Result == []
+#     Result = person_query(["hats"])
+#     assert Result == []
+#
+# @profile
+# def test_historical_query():
+#     #Valid Tests
+#     Result = historical_query("1970", "June", ["Oil"])
+#     assert type(Result) == list
+#     Result = historical_query("1980", "January", ["Bonds"])
+#     assert type(Result) == list
+#
+#     #Invalid Test
+#     Result = historical_query(1, 2, "cow")
+#     assert Result == []
+#     Result = historical_query(1980, "June", "Covid")
+#     assert Result == []
+#     Result = historical_query("1980", "June", [])
+#     assert Result == []
+#
+# # ==================
+# # Ends Tests
+# # ==================
+#
+#
+# #Print header for report
+# def printTitle():
+#     print()
+#     print("===NEWS FEED TESTS===")
+#     print()
+#
+# # Include all test functions here to use memory and CPU profilers
+# def main():
+#     printTitle()
+#     tracemalloc.start()# Start trace malloc
+#     test_keyword_query()
+#     test_person_query()
+#     test_historical_query()
+#
+#     # Get snapshot
+#     snapshot = tracemalloc.take_snapshot()
+#     top_stats = snapshot.statistics('lineno')
+#     print("[ Top 5 ]")
+#     for stat in top_stats[:5]:
+#         print(stat)
+#
+# if __name__ == '__main__':
+#     cProfile.run('main()')
--- nlp_analyzer/tests/test_nlp_analyzer.py
+++ nlp_analyzer/tests/test_nlp_analyzer.py
@@ -1,107 +1,107 @@
-# Tests for nlp module
-from memory_profiler import profile
-import tracemalloc
-import cProfile
-import re
-from nlpanalyzer import *
-
-
-# ==================
-# Start Tests
-# ==================
-
-# Dummy test to test workflow
-@profile
-def test_analyze_sent():
-
-    #Valid Tests
-    j = analyze_sentiment("text")
-    assert type(j) is int
-    j = analyze_sentiment("more text")
-    assert type(j) is int
-
-    #Failure Tests
-    k = analyze_sentiment(7)
-    assert k == ""
-    k = analyze_sentiment([])
-    assert k == ""
-
-@profile
-def test_analyze_ent():
-
-    #Success Tests
-    j = analyze_entity("John Doe is Great")
-    assert type(j) is list
-    j = analyze_entity("I love Nascar")
-    assert type(j) is list
-
-     #Failure Tests
-    k = analyze_entity(7)
-    assert k == []
-    k = analyze_entity(None)
-    assert k == []
-
-@profile
-def test_analyze_ent_sent():
-
-    #Success Tests
-    j = analyze_entity_sentiment("John Doe is Great")
-    assert type(j) is list
-    j = analyze_entity_sentiment("Denzel Washington is awful")
-    assert type(j) is list
-
-     #Failure Tests
-    k = analyze_entity_sentiment(7)
-    assert k == []
-    k = analyze_entity_sentiment({"Hello"})
-    assert k == []
-
-@profile
-def test_classify_content():
-    #Success Tests
-    j = classify_content("John Doe is Great")
-    assert type(j) is list
-    if j:
-        assert type(j[0]) is dict
-    j = classify_content("Tractors, Tractors, Tractors")
-    if j:
-        assert type(j[0]) is list
-
-     #Failure Test
-    k = classify_content(None)
-    assert k == []
-    k = classify_content(16)
-    assert k == []
-
-# ==================
-# Ends Tests
-# ==================
-
-#Included to show output from CPU and mem usage
-def test_main():
-    main()
-
-#Print header for report
-def printTitle():
-    print()
-    print("===NLP ANALYZER TESTS===")
-    print()
-
-# Include all test functions here to use memory and CPU profilers
-def main():
-    printTitle()
-    tracemalloc.start()# Start trace malloc
-    test_analyze_sent()
-    test_analyze_ent()
-    test_analyze_ent_sent()
-    test_classify_content()  
-
-    # Get snapshot
-    snapshot = tracemalloc.take_snapshot()
-    top_stats = snapshot.statistics('lineno')
-    print("[ Top 5 ]")
-    for stat in top_stats[:5]:
-        print(stat)
-
-if __name__ == '__main__':
-    cProfile.run('main()')
+# # Tests for nlp module
+# from memory_profiler import profile
+# import tracemalloc
+# import cProfile
+# import re
+# from nlpanalyzer import *
+#
+#
+# # ==================
+# # Start Tests
+# # ==================
+#
+# # Dummy test to test workflow
+# @profile
+# def test_analyze_sent():
+#
+#     #Valid Tests
+#     j = analyze_sentiment("text")
+#     assert type(j) is int
+#     j = analyze_sentiment("more text")
+#     assert type(j) is int
+#
+#     #Failure Tests
+#     k = analyze_sentiment(7)
+#     assert k == ""
+#     k = analyze_sentiment([])
+#     assert k == ""
+#
+# @profile
+# def test_analyze_ent():
+#
+#     #Success Tests
+#     j = analyze_entity("John Doe is Great")
+#     assert type(j) is list
+#     j = analyze_entity("I love Nascar")
+#     assert type(j) is list
+#
+#      #Failure Tests
+#     k = analyze_entity(7)
+#     assert k == []
+#     k = analyze_entity(None)
+#     assert k == []
+#
+# @profile
+# def test_analyze_ent_sent():
+#
+#     #Success Tests
+#     j = analyze_entity_sentiment("John Doe is Great")
+#     assert type(j) is list
+#     j = analyze_entity_sentiment("Denzel Washington is awful")
+#     assert type(j) is list
+#
+#      #Failure Tests
+#     k = analyze_entity_sentiment(7)
+#     assert k == []
+#     k = analyze_entity_sentiment({"Hello"})
+#     assert k == []
+#
+# @profile
+# def test_classify_content():
+#     #Success Tests
+#     j = classify_content("John Doe is Great")
+#     assert type(j) is list
+#     if j:
+#         assert type(j[0]) is dict
+#     j = classify_content("Tractors, Tractors, Tractors")
+#     if j:
+#         assert type(j[0]) is list
+#
+#      #Failure Test
+#     k = classify_content(None)
+#     assert k == []
+#     k = classify_content(16)
+#     assert k == []
+#
+# # ==================
+# # Ends Tests
+# # ==================
+#
+# #Included to show output from CPU and mem usage
+# def test_main():
+#     main()
+#
+# #Print header for report
+# def printTitle():
+#     print()
+#     print("===NLP ANALYZER TESTS===")
+#     print()
+#
+# # Include all test functions here to use memory and CPU profilers
+# def main():
+#     printTitle()
+#     tracemalloc.start()# Start trace malloc
+#     test_analyze_sent()
+#     test_analyze_ent()
+#     test_analyze_ent_sent()
+#     test_classify_content()
+#
+#     # Get snapshot
+#     snapshot = tracemalloc.take_snapshot()
+#     top_stats = snapshot.statistics('lineno')
+#     print("[ Top 5 ]")
+#     for stat in top_stats[:5]:
+#         print(stat)
+#
+# if __name__ == '__main__':
+#     cProfile.run('main()')
